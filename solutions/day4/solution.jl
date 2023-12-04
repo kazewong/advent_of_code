5d8a9f3bdf6239f37715e58a25586646f0adb1f1
kazewong <kazewong.physics@gmail.com>
2023-12-04 14:33:04 -0500

    upload day4

--- solutions/day4/solution.jl
+++ solutions/day4/solution.jl
@@ -7,8 +7,6 @@ struct Card
     choice_number::Vector{Int}
 end
 
-# Part1
-
 function parse_card(input::String)
     card, numbers = split(input, ":")
     card_number = parse(Int, last(split(card, " ")))
@@ -17,6 +15,10 @@ function parse_card(input::String)
     choice_number = filter(x->x!="",split(choice_number, " "))
     return Card(card_number, parse.(Int, winning_number), parse.(Int, choice_number))
 end
+
+cards = parse_card.(data)
+
+# Part1
 
 function check_card(card::Card)
     checks = map(x -> x ∈ card.winning_number, card.choice_number)
@@ -29,5 +31,22 @@ function check_card(card::Card)
     return score
 end
 
-cards = parse_card.(data)
-sum(check_card.(cards))
+ans_part1 = sum(check_card.(cards))
+
+# Part2
+
+function number_of_wins(card::Card)
+    checks = map(x -> x ∈ card.winning_number, card.choice_number)
+    return sum(checks)
+end
+
+function check_card_copies(card::Card, win_table::Vector{Int})
+    number_of_card_gen = 1
+    for i in (1:win_table[card.number])
+        number_of_card_gen += check_card_copies(cards[card.number+i], win_table)
+    end
+    return number_of_card_gen
+end
+
+win_table = number_of_wins.(cards)
+ans_part2 = sum(map(x -> check_card_copies(x, win_table), cards))
