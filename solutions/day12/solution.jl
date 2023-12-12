e4d114be323ede673aa87287abc2d7b0559adaf1
kazewong <kazewong.physics@gmail.com>
2023-12-12 17:00:09 -0500

    "#" != '#' in julia

--- solutions/day12/solution.jl
+++ solutions/day12/solution.jl
@@ -9,37 +9,37 @@ end
 
 function count_arrangement(position::Int, group::Int, record::String, info::Vector{Int})
 
-    println(position," ",group, " ", length(record), " ", length(info))
+    # println(position," ",group, " ", length(record), " ", length(info))
     if group > length(info)
         if (position < length(record)-1) && ('#' ∈ record[(position+1):end])
             # Run out of groups but still have some broken spring left
-            println("+0")
+            # println("+0")
             return 0
         else
-            println("+1")
+            # println("+1")
             return 1
         end
     end
 
-    if position >= length(record)-1
+    if position > length(record)-1
         # Run out of record but still have some groups left
         return 0
     end
 
-    println(record[position])
+    # println(record[position])
 
     # minimum([length(record),position+info[group]])
 
     if record[position] == '?'
         # This spring is broken and start the group here
-        if ('.' ∉ record[position:minimum([length(record),position+info[group]-1])]) && (record[minimum([length(record),position+info[group]])] != "#")
+        if ('.' ∉ record[position:minimum([length(record),position+info[group]-1])]) && (record[minimum([length(record),position+info[group]])] != '#')
             return count_arrangement(position + info[group]+1, group + 1, record, info) + count_arrangement(position + 1, group, record, info)
         else
             # This spring is operational
             return count_arrangement(position + 1, group, record, info)
         end
     elseif record[position] == '#'
-        if ('.' ∉ record[position:minimum([length(record),position+info[group]-1])]) && (record[minimum([length(record),position+info[group]])] != "#")
+        if ('.' ∉ record[position:minimum([length(record),position+info[group]-1])]) && (record[minimum([length(record),position+info[group]])] != '#')
             return count_arrangement(position + info[group]+1, group + 1, record, info)
         else
             return 0          
@@ -51,5 +51,5 @@ end
 
 lines = map(parse_line, data)
 count_arrangement(1,1,lines[1][1],lines[1][2])
-count_arrangement(1,1,lines[6][1],lines[6][2])
-map(x->count_arrangement(1,1,x[1],x[2]),lines)
+count_arrangement(1,1,lines[3][1],lines[3][2])
+result = sum(map(x->count_arrangement(1,1,x[1],x[2]),lines))
