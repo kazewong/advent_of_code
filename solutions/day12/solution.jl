4b03e96067656b615da849b583ec1873b966e9ab
Kaze Wong <kazewong.physics@gmail.com>
2023-12-12 20:47:46 -0500

    Fix input file path in solution.jl

--- solutions/day12/solution.jl
+++ solutions/day12/solution.jl
@@ -1,5 +1,5 @@
-# data = readlines("./solutions/day12/input.txt")
-data = readlines("./solutions/day12/test_input.txt")
+data = readlines("./solutions/day12/input.txt")
+# data = readlines("./solutions/day12/test_input.txt")
 
 function parse_line(line::String)
     record, info = split(line, " ")
@@ -9,14 +9,14 @@ end
 
 function count_arrangement(position::Int, group::Int, record::String, info::Vector{Int})
 
-    # println(position," ",group, " ", length(record), " ", length(info))
+    println(position," ",group, " ", length(record), " ", length(info))
     if group > length(info)
-        if (position < length(record)-1) && ('#' ∈ record[(position+1):end])
+        if (position < length(record)) && ('#' ∈ record[position:end])
             # Run out of groups but still have some broken spring left
             # println("+0")
             return 0
         else
-            # println("+1")
+            println("+1")
             return 1
         end
     end
@@ -31,15 +31,21 @@ function count_arrangement(position::Int, group::Int, record::String, info::Vect
     # minimum([length(record),position+info[group]])
 
     if record[position] == '?'
+        if (position + info[group] - 1) >= length(record)
+            # This spring cannot be broken since there is no enough space for the group
+            return 0
         # This spring is broken and start the group here
-        if ('.' ∉ record[position:minimum([length(record),position+info[group]-1])]) && (record[minimum([length(record),position+info[group]])] != '#')
+        elseif ('.' ∉ record[position:position+info[group]-1]) && (record[position+info[group]] != '#')
             return count_arrangement(position + info[group]+1, group + 1, record, info) + count_arrangement(position + 1, group, record, info)
         else
             # This spring is operational
             return count_arrangement(position + 1, group, record, info)
         end
     elseif record[position] == '#'
-        if ('.' ∉ record[position:minimum([length(record),position+info[group]-1])]) && (record[minimum([length(record),position+info[group]])] != '#')
+        if (position + info[group] - 1) >= length(record)
+            # This spring cannot be broken since there is no enough space for the group
+            return 0 
+        elseif ('.' ∉ record[position:position+info[group]-1]) && (record[position+info[group]] != '#')
             return count_arrangement(position + info[group]+1, group + 1, record, info)
         else
             return 0          
@@ -51,5 +57,5 @@ end
 
 lines = map(parse_line, data)
 count_arrangement(1,1,lines[1][1],lines[1][2])
-count_arrangement(1,1,lines[3][1],lines[3][2])
-result = sum(map(x->count_arrangement(1,1,x[1],x[2]),lines))
+count_arrangement(1,1,lines[2][1],lines[2][2])
+past1_ans = sum(map(x->count_arrangement(1,1,x[1],x[2]),lines))
