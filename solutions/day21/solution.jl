c71b19d46ea3c0a30f69edbd5736c732da0f3846
kazewong <kazewong.physics@gmail.com>
2023-12-21 11:25:17 -0500

    part2

--- solutions/day21/solution.jl
+++ solutions/day21/solution.jl
@@ -1,6 +1,6 @@
 
-# data = readlines("./solutions/day21/input.txt")
-data = readlines("./solutions/day21/test_input.txt")
+data = readlines("./solutions/day21/input.txt")
+# data = readlines("./solutions/day21/test_input.txt")
 
 mutable struct Tile
     type::Bool
@@ -24,8 +24,10 @@ end
 function print_grid(grid::Dict{CartesianIndex{2}, Tile})
     x_max = maximum([i[1] for i in keys(grid)])
     y_max = maximum([i[2] for i in keys(grid)])
-    for y in 1:y_max
-        for x in 1:x_max
+    x_min = minimum([i[1] for i in keys(grid)])
+    y_min = minimum([i[2] for i in keys(grid)])
+    for y in y_min:y_max
+        for x in x_min:x_max
             if grid[CartesianIndex((x, y))].reachable
                 print("O")
             else
@@ -43,10 +45,12 @@ end
 function propagate_grid!(grid::Dict{CartesianIndex{2}, Tile})
     x_max = maximum([i[1] for i in keys(grid)])
     y_max = maximum([i[2] for i in keys(grid)])
+    x_min = minimum([i[1] for i in keys(grid)])
+    y_min = minimum([i[2] for i in keys(grid)])
     reachable_tiles = filter(x -> x[2].reachable, grid)
     for (index, tile) in reachable_tiles
         for neighbor in CartesianIndex.([(index[1]+1, index[2]), (index[1]-1, index[2]), (index[1], index[2]+1), (index[1], index[2]-1)])
-            if (neighbor[1] > 0) && (neighbor[1] <= x_max) && (neighbor[2] > 0) && (neighbor[2] <= y_max) && (grid[neighbor].type)
+            if (neighbor[1] >= x_min) && (neighbor[1] <= x_max) && (neighbor[2] >= y_min) && (neighbor[2] <= y_max) && (grid[neighbor].type)
                 grid[neighbor].reachable = true
             end
         end
@@ -88,21 +92,22 @@ function ==(grid1::Dict{CartesianIndex{2}, Tile}, grid2::Dict{CartesianIndex{2},
     return compare_grid(grid1, grid2)
 end
 
-function fill_cycle_detector(grid::Dict{CartesianIndex{2}, Tile}, starting_point::CartesianIndex{2})
-    grid = deepcopy(grid)
-    grid[starting_point].reachable = true
-    temp_grid = Vector{Dict{CartesianIndex{2}, Tile}}()
-    push!(temp_grid, grid)
-    loop = true
-    while loop
-        grid = grid + 1
-        if grid in temp_grid
-            loop = false
-        end
-        push!(temp_grid, grid)
-    end
-    return length(temp_grid)-2
-end
+# Cycle detection is a red herring!
+# function fill_cycle_detector(grid::Dict{CartesianIndex{2}, Tile}, starting_point::CartesianIndex{2})
+#     grid = deepcopy(grid)
+#     grid[starting_point].reachable = true
+#     temp_grid = Vector{Dict{CartesianIndex{2}, Tile}}()
+#     push!(temp_grid, grid)
+#     loop = true
+#     while loop
+#         grid = grid + 1
+#         if grid in temp_grid
+#             loop = false
+#         end
+#         push!(temp_grid, grid)
+#     end
+#     return length(temp_grid)-2
+# end
 
 function reach_cycle_detector(grid::Dict{CartesianIndex{2}, Tile}, starting_point::CartesianIndex{2}, end_point::CartesianIndex{2})
     grid = deepcopy(grid)
@@ -118,9 +123,34 @@ function reach_cycle_detector(grid::Dict{CartesianIndex{2}, Tile}, starting_poin
     end
     return counter
 end
-    
+
+# repeat the grid
+function replicate_grid(grid::Dict{CartesianIndex{2}, Tile}, n::Int)
+    new_grid = deepcopy(grid)
+    x_max = maximum([i[1] for i in keys(grid)])
+    y_max = maximum([i[2] for i in keys(grid)])
+    for i in -n:n
+        for j in -n:n
+            for (index, tile) in grid
+                index = CartesianIndex((index[1]+x_max*i, index[2]+y_max*j))
+                new_grid[index] = deepcopy(tile)
+            end
+        end
+    end
+    return new_grid
+end
+
 
 grid, starting_point = parse_data(data)
-# grid[starting_point].reachable = true
-cycles = cycle_detector(grid, CartesianIndex((11,11)))
-reach = map(x->reach_cycle_detector(grid, CartesianIndex((1,1)), CartesianIndex((x,1))), 1:11)
+big_grid = replicate_grid(grid, 6)
+a1, new_grid = propagate_and_count(big_grid, 65, starting_point)
+a2, new_grid = propagate_and_count(big_grid, 196, starting_point)
+a3, new_grid = propagate_and_count(big_grid, 327, starting_point)
+
+a = (a1 - 2*a2 + a3)/2
+b = (-3*a1 + 4*a2 - a3) /2
+c = a1
+n = 26501365÷131
+
+part2_ans = a*n^2 + b*n + c
+
