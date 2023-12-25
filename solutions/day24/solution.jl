cb708ddb2037ec229b4ee1fbafd19d38351fc0d0
Kaze Wong <kazewong.physics@gmail.com>
2023-12-24 21:09:44 -0500

    Symbolics.jl felt like cheating

--- solutions/day24/solution.jl
+++ solutions/day24/solution.jl
@@ -55,3 +55,34 @@ data = readlines("./solutions/day24/input.txt")
 # data = readlines("./solutions/day24/test_input.txt")
 hails = parse_data(data)
 counts, intercept_array = count_cross(hails, [200000000000000,400000000000000])
+
+part1_ans = counts
+
+# Part 2 
+
+using Symbolics
+using LinearAlgebra
+@variables x[1:3] dx[1:3]
+@variables a1[1:3] b1[1:3] a2[1:3] b2[1:3] a3[1:3] b3[1:3]
+
+eq1 = Symbolics.scalarize(cross(x .- a1, dx .- b1) .- cross(x .- a2, dx .- b2))
+eq2 = Symbolics.scalarize(cross(x .- a1, dx .- b1) .- cross(x .- a3, dx .- b3))
+
+coef_dict = Dict{Num, Int}()
+for i in 1:3
+    coef_dict[a1[i]] = hails[1].position[i] 
+    coef_dict[b1[i]] = hails[1].derivative[i]
+    coef_dict[a2[i]] = hails[2].position[i]
+    coef_dict[b2[i]] = hails[2].derivative[i]
+    coef_dict[a3[i]] = hails[6].position[i]
+    coef_dict[b3[i]] = hails[6].derivative[i] # Somehow using the first 3 is wrong, probably rounding error
+end
+
+eq_set = [eq1..., eq2...].~0
+
+result = Symbolics.solve_for(substitute(eq_set, coef_dict), [x..., dx...])
+part2_ans = result[1] + result[2] + result[3]
+
+using Printf
+@printf("Part 1 answer: %d\n", part1_ans)
+@printf("Part 2 answer: %d\n", part2_ans)
