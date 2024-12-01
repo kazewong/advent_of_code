3800314b73ece3164095e109b8393c1141399289
Kaze Wong <kazewong.physics@gmail.com>
2024-11-30 22:47:00 -0500

    Implement file reading functionality and update go.work to include day1 module

--- go/day1/main.go
+++ go/day1/main.go
@@ -1,2 +1,49 @@
-package day1
+package main
 
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func readFile(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var fileLines []string
+	for scanner.Scan() {
+		fileLines = append(fileLines, scanner.Text())
+	}
+	file.Close()
+	return fileLines
+}
+
+func part1() {
+	
+}
+
+func part2() {
+	
+}
+
+func main() {
+
+	var filename string = os.Args[1]
+
+	// Read file
+	fileLines := readFile(filename)
+
+	// Print the content of the file
+	fmt.Println("The content of the file is:")
+	for _, line := range fileLines {
+		fmt.Println(line)
+	}
+
+	// Print answer for part 1
+	fmt.Println("The answer for part 1 is:")
+	part1()
+	
+}
