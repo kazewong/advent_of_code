b77adb6b98a76f73c032fc29fc51f8e32020e487
Kaze Wong <kazewong.physics@gmail.com>
2024-12-05 12:28:00 -0500

    update rust solution

--- rust/day5/src/main.rs
+++ rust/day5/src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::fs;
 use std::env::args;
+use std::cmp::Ordering;
 
 fn read_input(str: String) -> Vec<String> {
     let result = fs::read_to_string(str).expect("Failed to read input.txt");
@@ -36,8 +37,7 @@ fn check_line(input: Vec<i32>, guide: HashMap<i32, Vec<i32>>) -> bool{
     let mut result = true;
     for i in 0..input.len() {
         let current = input[i];
-        let iter_len = i as i32 -1;
-        for j in 0..iter_len{
+        for j in 0..i.clone(){
             if guide.contains_key(&current){
                 if guide.get(&current).unwrap().contains(&input[j as usize]) {
                     result = false;
@@ -61,13 +61,37 @@ fn part1(input: Vec<String>, guide: Vec<String>) -> i32 {
     result
 }
 
+fn custom_compare(a: i32, b: i32, guide: HashMap<i32, Vec<i32>>) -> Ordering {
+    if guide.contains_key(&a) {
+        if guide.get(&a).unwrap().contains(&b) {
+            return Ordering::Less
+        }
+        else {
+            return Ordering::Equal
+        }
+    }
+    else {
+        return Ordering::Equal
+    }
+
+}
+
 fn part2(input: Vec<String>, guide: Vec<String>) -> i32{
-    0
+    let input = parse_input(input);
+    let guide = parse_guide(guide);
+    let incorrect = input.iter().filter(|x| !check_line(x.to_vec(), guide.clone())).collect::<Vec<&Vec<i32>>>();
+    let mut result = 0;
+    for line in incorrect {
+        let mut temp = line.clone();
+        temp.sort_by(|a, b| custom_compare(*a, *b, guide.clone()));
+        result += temp[temp.len()/2];
+    }
+    result
 }
 
 fn main(){
-    // let tag = args().nth(1).unwrap();
-    let tag = "../asset/day5/test".to_owned();
+    let tag = args().nth(1).unwrap();
+    // let tag = "../asset/day5/test".to_owned();
     let guide = read_input(tag.clone() + "_guide.txt");
     let input = read_input(tag + "_input.txt");
     println!("Input: --------");
