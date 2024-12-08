f30431d104cfc5873a464d9d7cf3aa69df8d3d8d
Kaze Wong <kazewong.physics@gmail.com>
2024-12-07 20:26:25 -0500

    implement part2 logic and add check_line_part2 function

--- rust/day7/src/main.rs
+++ rust/day7/src/main.rs
@@ -16,10 +16,10 @@ fn check_line(target: u64, components: Vec<u64>) -> bool {
             let addition = previous_reachable[j] + components[i];
             let multiplication = previous_reachable[j] * components[i];
             // if !previous_reachable.contains(&addition) {
-                current_reachable.push(addition);
+            current_reachable.push(addition);
             // }
             // if !previous_reachable.contains(&multiplication) {
-                current_reachable.push(multiplication);
+            current_reachable.push(multiplication);
             // }
         }
         previous_reachable = current_reachable.clone();
@@ -44,8 +44,36 @@ fn part1(input: Vec<String>) -> u64 {
     result
 }
 
+fn check_line_part2(target: u64, components: Vec<u64>) -> bool {
+    let mut previous_reachable: Vec<u64> = vec![components[0]];
+    let mut current_reachable: Vec<u64> = vec![];
+    for i in 1..components.len() {
+        current_reachable.clear();
+        for j in 0..previous_reachable.len() {
+            current_reachable.push(previous_reachable[j] + components[i]);
+            current_reachable.push(previous_reachable[j] * components[i]);
+            current_reachable.push(previous_reachable[j] * 10_u64.pow((components[i].ilog10() + 1).try_into().unwrap()) + components[i]);
+        }
+        previous_reachable = current_reachable.clone();
+    }
+    if current_reachable.contains(&target) {
+       return true;
+    } else {
+        return false;
+    }
+}
+
 fn part2(input: Vec<String>) -> u64{
-    0
+    let mut result = 0;
+    for line in input {
+        let (target, components) = line.split_once(": ").unwrap();
+        let target = target.parse::<u64>().unwrap();
+        let components: Vec<u64> = components.split(" ").map(|s| s.parse::<u64>().unwrap()).collect();
+        if check_line_part2(target, components) {
+            result += target;
+        }
+    }
+    result
 }
 
 fn main(){
